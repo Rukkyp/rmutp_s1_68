a7c1c239351ffb06d6894ebd9ee943adb7da118e
Ruksita Panya <ruksita-p@rmutp.ac.th>
2025-08-27 17:39:30 +0700

    add profile route and fetch data from db

--- app/index.ts
+++ app/index.ts
@@ -1,4 +1,8 @@
 import { Hono } from "hono";
+import { PrismaClient } from "@prisma/client";
+//import { PrismaClient } from "../generated/prisma/client";
+
+const prisma = new PrismaClient();
 
 const app = new Hono();
 
@@ -8,6 +12,10 @@ app.get("/about", (c) => {
     return c.json ({
         massage: "Ruksita Panya"
     });
+});
+app.get("/profile", async(c) => {
+    const profile = await prisma.profile.findMany();
+    return c.json(profile);
 });
 
 export default app;
--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,18 @@
-import { Hono } from "hono";
-const app = new Hono();
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const hono_1 = require("hono");
+const client_1 = require("@prisma/client");
+//import { PrismaClient } from "../generated/prisma/client";
+const prisma = new client_1.PrismaClient();
+const app = new hono_1.Hono();
 app.get("/", (c) => c.text("Hello World!"));
 app.get("/about", (c) => {
     return c.json({
         massage: "Ruksita Panya"
     });
 });
-export default app;
+app.get("/profile", async (c) => {
+    const profile = await prisma.profile.findMany();
+    return c.json(profile);
+});
+exports.default = app;
