89456d6345a6e1dcb46bce7e491e8eb5582f70fe
Ruksita Panya <ruksita-p@rmutp.ac.th>
2025-08-27 12:47:43 +0700

    create

--- dist/index.js
+++ dist/index.js
@@ -1,13 +1,9 @@
 import { Hono } from "hono";
-
 const app = new Hono();
-
 app.get("/", (c) => c.text("Hello World!"));
-
 app.get("/about", (c) => {
-    return c.json ({
+    return c.json({
         massage: "Ruksita Panya"
     });
 });
-
 export default app;
--- dist/server.js
+++ dist/server.js
@@ -1,7 +1,5 @@
 import { serve } from "@hono/node-server";
 import app from "./index.js";
-
 serve(app, (info) => {
     console.log(`Server is running on localhost:${info.port}`);
 });
-
